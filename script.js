22ada94c3eb31401698bd48ec95be51f315cc9e8
John <martian.halo@gmail.com>
2021-10-31 14:27:36 -0700

    debug

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var apiKey = "55fe3800";
+const apiKey = "55fe3800";
 
 document.querySelector("#submit-btn").addEventListener("click", function() {
     //capture user input
-    var userInput = document.querySelector("#search").value;
+    const userInput = document.querySelector("#search").value;
 
     //generate Weather info
     //movie(userInput);
     currentMovie(userInput);
 });
 
-var currentMovie = function(Title) {
-    fetch(`http://www.omdbapi.com/?t=${Title}&apikey=55fe3800`).then(function(res) {
+const currentMovie = function(Title) {
+    fetch(`https://www.omdbapi.com/?t=${Title}&apikey=${apiKey}`).then(function(res) {
         return res.json(res)
     }).then(function(data) {
         console.log(data);
-    var template = `
+    const template = `
     <div class = "text-center">
         <h1>${data.Title} ${data.Year}</h1>
         <img src = "${data.Poster}">
         <h5>${data.Rated}</h5>
-        <h5>${data.Ratings[1].Value} Rotten Tomatoes</h5>
+        <h5>${data.Ratings[0].Value} Rotten Tomatoes</h5>
         <h5>${data.Plot}</h5>
     </div>
 
